b26655db4a6ea49c844df635829ae719916db315
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2024-03-20 18:29:27 +0900

    update set seed

--- run_longExp.py
+++ run_longExp.py
@@ -99,6 +99,13 @@ if __name__ == '__main__':
     random.seed(fix_seed)
     torch.manual_seed(fix_seed)
     np.random.seed(fix_seed)
+    torch.cuda.manual_seed(fix_seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    # os.environ["PYTHONHASHSEED"] = str(fix_seed)
+    print(f"Random seed set as {fix_seed}")
 
 
     args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
